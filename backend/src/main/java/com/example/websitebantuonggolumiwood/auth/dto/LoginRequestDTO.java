73a8bef9c4da0c798a878663123e4abb7b1a0388
TÚ IT <dinhthanhtu26012005@gmail.com>
2025-06-08 11:03:43 +0700

    backup file

--- backend/src/main/java/com/example/websitebantuonggolumiwood/auth/dto/LoginRequestDTO.java
+++ backend/src/main/java/com/example/websitebantuonggolumiwood/auth/dto/LoginRequestDTO.java
@@ -1,4 +1,4 @@
-package com.example.websitebantuonggolumiwood;
+package com.example.websitebantuonggolumiwood.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
